e95a81453ca1e6e50f55a0d2719b94cf1998d85a
Evgeny Ukhanov <evgeny@aurora.dev>
2022-10-29 20:37:59 +0200

    Add parameters

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,30 @@
+use serde_derive::Deserialize;
+use std::env::args;
+
+#[derive(Deserialize, Debug)]
+pub struct ResultValues {
+    key: String,
+    value: String,
+}
+
+#[derive(Deserialize, Debug)]
+pub struct ResultData {
+    values: Vec<ResultValues>,
+}
+
+#[derive(Deserialize, Debug)]
+pub struct BlockData {
+    result: ResultData,
+}
+
 fn main() {
     println!(
         "Aurora Engine migration tool v{}",
         env!("CARGO_PKG_VERSION")
     );
+    let json_file = args().nth(1).expect("Expected json file");
+    let data = std::fs::read_to_string(json_file).expect("Failed read data");
+    let json_data: BlockData = serde_json::from_str(&data).expect("Failed read json");
+    println!("{:.3} Gb", data.len() as f64 / 1_000_000_000.);
+    println!("{:#?} items", json_data.result.values.len());
 }
