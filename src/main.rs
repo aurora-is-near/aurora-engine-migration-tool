981398b60807052648a248464eee5a168d1f20be
Evgeny Ukhanov <evgeny@aurora.dev>
2022-10-30 20:51:02 +0100

    Key prefix builder

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,29 @@
+use aurora_engine_types::storage::{EthConnectorStorageId, KeyPrefix, VersionPrefix};
 use serde_derive::Deserialize;
 use std::env::args;
 
 #[derive(Deserialize, Debug)]
 pub struct ResultValues {
-    key: String,
-    value: String,
+    pub key: String,
+    pub value: String,
 }
 
 #[derive(Deserialize, Debug)]
 pub struct ResultData {
-    values: Vec<ResultValues>,
+    pub values: Vec<ResultValues>,
 }
 
 #[derive(Deserialize, Debug)]
 pub struct BlockData {
-    result: ResultData,
+    pub result: ResultData,
+}
+
+pub fn bytes_to_key(prefix: KeyPrefix, bytes: &[u8]) -> Vec<u8> {
+    [&[u8::from(VersionPrefix::V1)], &[u8::from(prefix)], bytes].concat()
+}
+
+pub fn construct_contract_key(suffix: &EthConnectorStorageId) -> Vec<u8> {
+    bytes_to_key(KeyPrefix::EthConnector, &[u8::from(*suffix)])
 }
 
 fn main() {
@@ -27,4 +36,7 @@ fn main() {
     let json_data: BlockData = serde_json::from_str(&data).expect("Failed read json");
     println!("{:.3} Gb", data.len() as f64 / 1_000_000_000.);
     println!("{:#?} items", json_data.result.values.len());
+
+    //EthConnectorStorageId
+    //let mut v = construct_contract_key(&EthConnectorStorageId::UsedEvent).to_vec();
 }
